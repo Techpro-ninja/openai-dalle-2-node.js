fb815807ab2f5fe38a13ece108d5e92eba9fc12b
chilarai <chilly5476@gmail.com>
2023-02-19 16:50:58 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # openai-codex-with-nodejs
 
-A Node.js web application which interacts with OpenAI Codex model to write code and generate sql.
+A Node.js web application which interacts with OpenAI DALLE.E model to generate, edit images and generate variations.
 You can read more about the tutorial on my blog [openai-codex-with-node-js-tutorial](https://techpro.ninja/openai-codex-with-node-js-tutorial/)
 
 ## Installation & Running
--- index.js
+++ index.js
@@ -1,56 +1,125 @@
 const express = require("express");
 const cors = require("cors");
-
-// Import the OpenAI
-const { Configuration, OpenAIApi } = require("openai");
-
-// Create a new OpenAI configuration and paste your API key
-// obtained from Step 1
-// The key displayed here is a fake key
-const configuration = new Configuration({
-  apiKey: "sk-XXXXXXXTJ4UznFsQoZT3BlbkFJrPieBj36AZkmXXXXXXX",
-});
-const openai = new OpenAIApi(configuration);
-
 const bodyParser = require("body-parser");
+const handlebars = require("express-handlebars");
+const multer = require("multer");
+const fs = require("fs");
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.set("view engine", "hbs");
+app.use(express.static("public"));
+app.use(express.static("uploads"));
+app.set("view engine", "handlebars");
 
+const upload = multer({ dest: "uploads/" });
 const port = 3000;
 
-//Render the input form
+// Import the OpenAI
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+
+//Sets handlebars configurations (we will go through them later on)
+app.engine(
+  "handlebars",
+  handlebars.engine({
+    layoutsDir: __dirname + "/views/layouts",
+  })
+);
+
+// Generate image - - Get input
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("./partials/index", { layout: "master" });
 });
 
-// create a post route to receive input data
 app.post("/", async (req, res) => {
   const data = req.body;
 
-  // Query the OpenAI API
-  let promptContext = `/*
-    Javascript application
-    ${data.queryPrompt}
-    */
-    <!DOCTYPE html>
-   `;
-
-  const response = await openai.createCompletion({
-    model: "code-davinci-002",
-    prompt: `${promptContext}`,
-    temperature: 0,
-    max_tokens: 100,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0,
+  const response = await openai.createImage({
+    prompt: data.queryPrompt,
+    n: 1,
+    size: "256x256",
   });
-  res.json(response.data.choices[0].text);
+
+  image_url = response.data.data[0].url;
+
+  res.render("./partials/index", {
+    layout: "master",
+    data: image_url,
+  });
 });
 
-app.listen(port, () => {
-  console.log(`Node server listening at http://localhost:${port}`);
+// Upload image - - Get input
+app.get("/get-upload", (req, res) => {
+  res.render("./partials/getupload", { layout: "master" });
 });
+
+const cpUpload = upload.fields([
+  { name: "upload_image", maxCount: 1 },
+  { name: "upload_masked_image", maxCount: 1 },
+]);
+
+app.post("/get-upload", cpUpload, async function (req, res, next) {
+  const response = await openai.createImageEdit(
+    fs.createReadStream(req.files.upload_image[0].path),
+    fs.createReadStream(req.files.upload_masked_image[0].path),
+    req.body.queryPrompt,
+    1,
+    "256x256"
+  );
+
+  fs.unlink(req.files.upload_image[0].path, (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+
+  fs.unlink(req.files.upload_masked_image[0].path, (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+
+  image_url = response.data.data[0].url;
+  res.render("./partials/getupload", {
+    layout: "master",
+    data: image_url,
+    original: req.files.upload_image[0].filename,
+    mask: req.files.upload_masked_image[0].filename,
+  });
+});
+
+// Image Variation
+app.get("/get-variation", (req, res) => {
+  res.render("./partials/getvariation", { layout: "master" });
+});
+
+app.post(
+  "/get-variation",
+  upload.single("upload_image"),
+  async function (req, res, next) {
+    const response = await openai.createImageVariation(
+      fs.createReadStream(req.file.path),
+      1,
+      "256x256"
+    );
+
+    fs.unlink(req.file.path, (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+
+    image_url = response.data.data[0].url;
+    res.render("./partials/getvariation", {
+      layout: "master",
+      data: image_url,
+    });
+  }
+);
+
+app.listen(port, () => console.log(`App listening to port ${port}`));
